835a8f22e7a597c945f9c890d556741db1009345
EBogdanov2 <evgeny.b.jr@gmail.com>
2018-05-18 17:44:40 -0400

    added HSTS capability

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-const express = require('express')
-const app = express()
+const port = process.env.PORT || 8080
 
+var fs = require('fs');
+var hsts = require('hsts');
+var helmet = require('helmet');
+var express = require('express');
+var http = require('http');
+var express_enforces_ssl = require('express-enforces-ssl');
 var path = require("path");
+
+var app = express();
+
+app.enable('trust proxy');
+
+app.use(express_enforces_ssl());
+
+app.use(hsts({
+  // Must be at least 1 year to be approved
+  maxAge: 31536000,
+
+  // Must be enabled to be approved
+  includeSubDomains: true,
+  preload: true
+}))
 
 app.use(express.static(__dirname + '/public'));
 
-app.get( '/', function( req, res ) {
-    res.sendFile( path.join( __dirname + '/landing.html' ));
-  });
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + "/" + "landing.html");
+});
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+http.createServer(app).listen(port, function() {
+	console.log(port);
+});
